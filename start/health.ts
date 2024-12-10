df0e74970e104aecd4c23f07944b99620b0910c4
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-12-10 14:29:39 +0100

    feat(health): remove unused health checks

--- start/health.ts
+++ start/health.ts
@@ -1,9 +1,7 @@
-import { HealthChecks, DiskSpaceCheck, MemoryHeapCheck } from '@adonisjs/core/health'
+import { HealthChecks } from '@adonisjs/core/health'
 import { DbCheck } from '@adonisjs/lucid/database'
 import db from '@adonisjs/lucid/services/db'
 
 export const healthChecks = new HealthChecks().register([
-  new DiskSpaceCheck(),
-  new MemoryHeapCheck(),
   new DbCheck(db.connection()),
 ])
